ca42c9204f3eeb364a0738560beef1a1c0e68359
Vlad Eliseev <v.eliseev@auraband.io>
2024-04-02 20:47:28 +0300

    Initial commit

---
+++ Homework/HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkApp.swift
+//  Homework
+//
+//  Created by Vlad Eliseev on 02.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
